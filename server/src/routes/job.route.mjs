07b4558da102b53b0229486587bd73ba7bd4558b
TheGrayGodfather <milanmohapatra.official@gmail.com>
2024-02-14 22:47:12 +0530

    renameing route

--- server/src/routes/job.route.mjs
+++ server/src/routes/job.route.mjs
@@ -2,11 +2,11 @@ import express from "express";
 import jobController from "../controllers/job.controller.mjs";
 import { authenticateUser, authorizeUser } from "../middlewares/auth.mjs";
 import { ROLES } from "../utils/constant.mjs";
-const jobRoute = express.Router();
+const route = express.Router();
 
-jobRoute.use(authenticateUser);
-jobRoute.route("/").get(authorizeUser([ROLES.CANDIDATE]), jobController.getAllJobs);
-jobRoute.route("/").post(authorizeUser([ROLES.RECRUITER]), jobController.createAJob);
-jobRoute.route("/").post(authorizeUser([ROLES.CANDIDATE]), jobController.applyForJob);
+route.use(authenticateUser);
+route.route("/").get(authorizeUser([ROLES.CANDIDATE]), jobController.getAllJobs);
+route.route("/").post(authorizeUser([ROLES.RECRUITER]), jobController.createAJob);
+route.route("/").post(authorizeUser([ROLES.CANDIDATE]), jobController.applyForJob);
 
-export default jobRoute;
+export default route;
